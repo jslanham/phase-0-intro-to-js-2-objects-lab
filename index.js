79dc161f89c92b29492150f16829bdda5cfd1ecd
Joseph Lanham <jslanham2020@gmail.com>
2024-01-09 20:04:50 -0500

    all types of employee modification

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name : "name",
+    streetAddress : "road",
+}
+function updateEmployeeWithKeyAndValue(employeeObject, key, value){
+    employeeObject = {...employee};
+    employeeObject[key] = value;
+    return employeeObject;
+}
+function  destructivelyUpdateEmployeeWithKeyAndValue(employeeObject, key, value){
+    employee[key] = value;
+    return employee;
+    
+}
+function  deleteFromEmployeeByKey(employeeObject, key){
+    employeeObject = {...employee};
+    delete employeeObject[key];
+    return employeeObject;
+}
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key){
+    delete employee[key]
+    return employee
+}
